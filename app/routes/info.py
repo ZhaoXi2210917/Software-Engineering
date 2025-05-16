43e946e714e93844a6cadc55fd955460e23221b2
赵乐 <1726096127@qq.com>
2025-05-16 21:47:35 +0800

    优化info后端

--- app/routes/info.py
+++ app/routes/info.py
@@ -27,17 +27,8 @@ def get_species():
 # 主要信息
 @bp.route('/info')
 def info():
-    species_data = get_species()
-    species_json = json.dumps(species_data)
-    return render_template('info.html', species_data = species_json)
+    # 鱼群种类信息 用于饼图
+    raw_data = get_species()
+    species_data = [{'name':item['species'],'value':item['count']} for item in raw_data]
+    return render_template('info.html', species_data = species_data)
 
-# @bp.route('/pie-data')
-# def pie_data():
-#     species_data = get_species()
-#     return jsonify(species_data)
-
-# if __name__ == '__main__':
-#     result = get_species()
-#     print(type(result))
-#     for row in result:
-#         print(f"种类：{row['species']}数量：{row['count']}")
